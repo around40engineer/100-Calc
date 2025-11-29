6ae3340b624be971717460496669ec547b927622
around40engineer <around40engineer@gmail.com>
2025-11-30 03:12:16 +0900

    vite.config.tsを変更

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/kiro/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/100-Calc/' : '/',
 })
